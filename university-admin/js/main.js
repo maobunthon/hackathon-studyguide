c2c5ae5a759bab27196c4312cb92b70808a09ce5
Danet Krueng <danet.krueng@gmail.com>
2016-11-27 09:40:06 +0700

    [TASK] Add admin page for university profile

---
+++ university-admin/js/main.js
@@ -0,0 +1,49 @@
+$(function () {
+  var barChartCanvas = $("#barChart").get(0).getContext("2d");
+  var barChart = new Chart(barChartCanvas);
+  var barChartData = {
+    labels: ["May", "June", "July", "August", "September", "October", "November"],
+    datasets: [
+      {
+        label: "Visitors",
+        fillColor: "rgba(60,141,188,0.9)",
+        strokeColor: "rgba(60,141,188,0.8)",
+        pointColor: "#3b8bba",
+        pointStrokeColor: "rgba(60,141,188,1)",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(60,141,188,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  };
+  var barChartOptions = {
+    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+    scaleBeginAtZero: true,
+    //Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines: true,
+    //String - Colour of the grid lines
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+    //Number - Width of the grid lines
+    scaleGridLineWidth: 1,
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+    //Boolean - If there is a stroke on each bar
+    barShowStroke: true,
+    //Number - Pixel width of the bar stroke
+    barStrokeWidth: 1,
+    //Number - Spacing between each of the X value sets
+    barValueSpacing: 50,
+    //Number - Spacing between data sets within X values
+    barDatasetSpacing: 1,
+    //String - A legend template
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+    //Boolean - whether to make the chart responsive
+    responsive: true,
+    maintainAspectRatio: true
+  };
+
+  barChartOptions.datasetFill = false;
+  barChart.Bar(barChartData, barChartOptions);
+});
